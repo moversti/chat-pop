6d7a988d6df826e833490d9cd7ec35a646199718
Mikko Översti <moversti92@gmail.com>
2019-05-16 15:17:34 +0300

    refractor message styles to own file

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './Message.css'
 
-export default function Message({icon, message, timeName}) {
-    return (
-        <div className="chat-bubble">
-        
-        <FontAwesomeIcon icon={icon} size="2x" />
-        <div className="time-name">{timeName}</div>
-        <div className="content">{message}</div>
-        </div>
-    )
-  }
+export default function Message({ icon, message, timeName }) {
+  return (
+    <div className="chat-bubble">
+      <FontAwesomeIcon icon={icon} size="2x" />
+      <div className="time-name">{timeName}</div>
+      <div className="content">{message}</div>
+    </div>
+  );
+}
