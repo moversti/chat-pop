011de68ed78285ab66d3b627a0ae29acf1ec5571
Mikko Översti <moversti92@gmail.com>
2019-05-16 10:33:46 +0300

    Only draggable on handle

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -7,9 +7,10 @@ import './Chat.css'
 
 export default function Chat({ click }) {
   return (
-    <Draggable>
+    <Draggable handle="#handle">
       <div style={{border: '2px solid black', width:'150px', backgroundColor:'lightgrey', height:'350px'}}>
-        <div style={{border: '1px solid black', width:'148px', backgroundColor:'purple', height:'30px', left:'1px'}}>
+        <div style={{border: '1px solid black', width:'148px', backgroundColor:'purple', height:'30px', left:'1px'}}
+        id='handle'>
           <div onClick={click} style={{float:'right', marginRight:'4px', height:'100%'}}>
             <FontAwesomeIcon icon={faWindowMinimize} />
           </div>
