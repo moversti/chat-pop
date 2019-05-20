a1c830270081b567c614ebe973e27d23b354669d
Mikko Översti <moversti92@gmail.com>
2019-05-20 09:58:24 +0300

    firebase database

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyDTw0Li4ov52btSmxz7ze8UpQskznSMPTg",
+    authDomain: "chat-pop.firebaseapp.com",
+    databaseURL: "https://chat-pop.firebaseio.com",
+    projectId: "chat-pop",
+    storageBucket: "chat-pop.appspot.com",
+    messagingSenderId: "147424514677",
+    appId: "1:147424514677:web:4e48f4185b914820"
+  };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database()
+export default db;
